7af3c194bd71f7e16b64fb7c11bd6f68a6ed6b9e
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-05-20 19:54:24 -0700

    commit tree e90388dc807f97b063a480c8106a04828fd6b0c5

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 2;
+let num4 = 8;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num4 / num3;
+
+const max = Math.max(5, 10, 15, 20);
